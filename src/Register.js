4fa247dc419c79e4016b0ad362953085858c4efe
brian <brian.meyer@10up.com>
2020-03-05 13:32:19 -0700

    add register form, password validation

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import RegisterForm from "./components/register-form";
+
 function Register() {
-  return <h1>Register</h1>;
+  return (
+    <>
+      <h1>Your Application has been approved</h1>
+      <p>Please register for a new account</p>
+      <RegisterForm />
+    </>
+  );
 }
 
 export default Register;
---
+++ src/components/register-form.js
@@ -0,0 +1,128 @@
+import React from "react";
+
+import { Formik, Form, Field } from "formik";
+
+import * as Yup from "yup";
+
+const RegisterSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(8, "Username must be at least 8 characters")
+    .max(36, "Username can't exceed 36 characters")
+    .typeError("Username must contain letters")
+    .required("Please enter a username"),
+  password: Yup.string()
+    .min(
+      8,
+      "Password must contain at least 8 letters, one number, and one special character"
+    )
+    .max(30, "Exceeded field limit")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "Password must contain at least 8 letters, one number, and one special character"
+    )
+    .required("Please enter passsword"),
+  confirmPassword: Yup.string()
+    .min(
+      8,
+      "Password must contain at least 8 letters, one number, and one special character"
+    )
+    .max(30, "Exceeded field limit")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "Password must contain at least 8 letters, one number, and one special character"
+    )
+    .required("Please enter passsword")
+    .test("passwords-match", "Passwords must match", function(value) {
+      return this.parent.password === value;
+    })
+});
+
+function registerForm() {
+  function submitResponse(submission) {
+    console.log(submission);
+  }
+
+  return (
+    <Formik
+      initialValues={{
+        username: "",
+        password: "",
+        confirmPassword: ""
+      }}
+      validationSchema={RegisterSchema}
+      onSubmit={submittal => {
+        submitResponse(submittal);
+      }}
+      validateOnChange={false}
+      validateOnBlur={false}
+    >
+      {({ errors, touched, validateForm }) => (
+        <Form className="qualification-form">
+          <div className="field-group">
+            <label htmlFor="username">Username</label>
+            <div
+              className={errors.username && touched.username ? " -error" : ""}
+            >
+              <Field
+                id="username"
+                name="username"
+                placeholder="username"
+                type="text"
+              />
+            </div>
+            {errors.username && touched.username && (
+              <span className="error">{errors.username}</span>
+            )}
+          </div>
+          <div className="field-group">
+            <label htmlFor="password">Password</label>
+            <div
+              className={errors.password && touched.password ? "-error" : ""}
+            >
+              <Field
+                id="password"
+                name="password"
+                placeholder="password"
+                type="text"
+              />
+            </div>
+            {errors.password && touched.password && (
+              <span className="error">{errors.password}</span>
+            )}
+          </div>
+
+          <div className="field-group">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <div
+              className={
+                errors.confirmPassword && touched.confirmPassword
+                  ? "-error"
+                  : ""
+              }
+            >
+              <Field
+                id="confirmPassword"
+                name="confirmPassword"
+                placeholder="confirm password"
+                type="text"
+              />
+            </div>
+            {errors.confirmPassword && touched.confirmPassword && (
+              <span className="error">{errors.confirmPassword}</span>
+            )}
+          </div>
+
+          <p className="form-copy">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            autem in illum.
+          </p>
+          <button type="submit" onClick={() => validateForm()}>
+            Register
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default registerForm;
