d6752e4177d892e8d51a248071220376deb91593
brian <brian.meyer@10up.com>
2020-03-05 13:37:30 -0700

    Improve niceness of deny message, responsive styles

--- src/Deny.js
+++ src/Deny.js
@@ -3,7 +3,7 @@ import React from "react";
 function Deny(props) {
   return (
     <>
-      <h1>Unfortnately your application has been rejected</h1>
+      <h1>Your application has been denied</h1>
       <p>{props.location.state.message}</p>
       <p>For more information, please contact us at 888-444-5566</p>
     </>
